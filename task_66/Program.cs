c33e524bfd4e52422ffea14fcb85fc6ca9b21975
slava <slavayancheg@gmail.com>
2023-01-06 16:48:28 +0600

    init

---
+++ task_66/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N и нажмите enter:");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите M и нажмите enter:");
+            var m = int.Parse(Console.ReadLine());
+            if(n>=m)
+            {
+                Console.WriteLine("N должна быть меньше M");
+            }
+            else
+            {
+                var sum = 0;
+                Console.WriteLine(string.Format("сумма = {0}", RecursionSum(sum, n, m)));
+            }
+            Console.WriteLine("Нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+
+        static int RecursionSum(int sum, int n, int m)
+        {
+            if(n > m)
+                return sum;
+            return RecursionSum(sum + n, ++n, m);
+        }
+    }
+}
---
+++ task_68/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task_68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N и нажмите enter:");
+            var n = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Введите M и нажмите enter:");
+            var m = uint.Parse(Console.ReadLine());
+            if(n< 0 || m < 0)
+            {
+                Console.WriteLine("N и M должны быть больше или равно 0");
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Число Аккерамана = {0}", Ackerman(n, m)));
+            }
+            Console.WriteLine("Нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+
+        static uint Ackerman(uint n, uint m)
+        {
+            if(n == 0)
+                return m + 1;
+            else if(m == 0)
+                return Ackerman(n - 1, 1);
+            else
+                return Ackerman(n - 1, Ackerman(n, m - 1));
+        }
+    }
+}
